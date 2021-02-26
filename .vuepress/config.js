d13f61da1df5f98f8785b21cdc5c5656a9e262b4
libg <664120843@qq.com>
2021-02-26 16:58:29 +0800

    no message

---
+++ .vuepress/config.js
@@ -0,0 +1,117 @@
+module.exports = {
+    dest: "docs",
+    base: "/",
+    markdown: {
+        externalLinks: {
+            target: "_blank",
+            rel: "noopener noreferrer"
+        }
+    },
+    locales: {
+        "/": {
+            lang: "zh-CN",
+            title: "Berg",
+            description: "快速构建web应用程序"
+        },
+        //   "/en/": {
+        //   }
+    },
+    head: [
+
+    ],
+    themeConfig: {
+        locales: {
+            "/": {
+                nav: [
+                    {
+                        text: "指南",
+                        link: "/guide/"
+                    },
+                    {
+                        text: "生态",
+                        items: [
+                            {
+                                text: "berg-boot【Spring Boot示例项目】",
+                                link: "https://gitee.com/Nyberg/berg-boot",
+                            },
+                            {
+                                text: "berg-boot-sample【Spring Boot简单示例项目】",
+                                link: "https://gitee.com/Nyberg/berg-boot-sample",
+                            },
+                            {
+                                text: "berg-cloud【Spring Cloud示例项目】",
+                                link: "https://gitee.com/Nyberg/berg-cloud",
+                            },
+                            {
+                                text: "berg-wx-mp【微信公众号示例项目】",
+                                link: "https://gitee.com/Nyberg/berg-wx-mp",
+                            },
+                            {
+                                text: "berg-wx-miniapp【微信小程序示例项目】",
+                                link: "https://gitee.com/Nyberg/berg-wx-miniapp",
+                            },
+                            {
+                                text: "berg-wx-cp【微信企业号示例】",
+                                link: "https://gitee.com/Nyberg/berg-wx-cp",
+                            },
+                            {
+                                text: "ant-design-pro-berg【管理系统前端】",
+                                link: "https://gitee.com/Nyberg/ant-design-pro-berg",
+                            },
+                        ],
+                    },
+                    {
+                        text: 'GitHub',
+                        link: 'https://github.com/BoGeManger/berg-boot.git'
+                    }, {
+                        text: 'Gitee',
+                        link: 'https://gitee.com/Nyberg/berg-boot.git'
+                    }
+                ],
+                sidebar: {
+                    "/guide/": genGuideSidebar(true),
+                }
+            },
+            // "/en/": {
+            // }
+        }
+    },
+    plugins: [
+        ['@vuepress/back-to-top', true]
+    ]
+};
+
+function genGuideSidebar() {
+    return [
+        {
+            title: "简介",
+            collapsable: false,
+            children: [""]
+        },
+        {
+            title: "环境搭建",
+            collapsable: false,
+            children: ["create/docker", "create/redis", "create/minio", "create/nginx"]
+        },
+        {
+            title: "使用说明",
+            collapsable: false,
+            children: ["use/page","use/newdb","use/generator","use/error-handler"]
+        },
+        {
+            title: "部署",
+            collapsable: false,
+            children: ["build/docker","build/sh"]
+        },
+        {
+            title: "FAQ",
+            collapsable: false,
+            children: ["faq/problem"]
+        },
+        {
+            title: "未完待续。。。",
+            collapsable: false,
+            children: []
+        }
+    ];
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# berg-docs
-berg系列文档
+---
+home: true
+actionText: 快速开始 →
+actionLink: /guide/
+features:
+  - title: 最新技术栈
+    details: 使用最流行的技术SpringBoot、SpringCloud、Minio、Shiro、Vue、XXL-JOB、Nacos、Seata、Mybatis Plus等。
+  - title: 前后端分离
+    details: 提供前后端分离的版本：使用的前端技术Vue、Ant Design Vue Pro后端SpringBoot完全分离的权限管理系统。
+  - title: 微服务版本
+    details: Spring Cloud示例项目，包含Nacoe服务中心、Gateway网关授权等一系列分布式组件模块。
+  - title: 微信版本
+    details: 包含微信企业号、微信公众号、微信小程序三部分示例项目，扩展WxJava一个服务可支持多个公众号或小程序、企业号。
+  - title: 代码生成器
+    details: 扩展Mybatis Plus Generator可实现自定义代码生成，一键生成业务模块。
+  - title: 功能完善
+    details: 内置完整的权限架构，包括：菜单、角色、用户、定时任务管理等一系列系统常规模块。
+---
